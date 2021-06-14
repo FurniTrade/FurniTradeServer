c89da006f432399aaf76aa92ff00014d2cc5795a
Gagandeep Kang <gskang@bu.edu>
2021-06-13 20:55:04 -0400

    add controllers

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = 4000;
+const authRoutes = require('./Controllers/AuthenticationController');
 
+app.use('/auth', authRoutes);
 app.get("/", (req, res) => {
-    res.send("Welcome to the FurniTrade server")
+    res.send("Welcome to the FurniTrade server"); 
 })
 
 app.listen(port, () => {

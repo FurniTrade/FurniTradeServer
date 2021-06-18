0ea732fcb9709727e23e303db5a29111743c0ca2
bjoernh2000 <bjoernh@bu.edu>
2021-06-18 17:31:06 -0400

    set up mysql connection

--- index.js
+++ index.js
@@ -2,6 +2,26 @@ const express = require('express');
 const app = express();
 const port = 4000;
 const authRoutes = require('./Controllers/AuthenticationController');
+const mysql = require('mysql');
+require('dotenv').config()
+
+
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: 'FurniTradeDB'
+});
+connection.connect((err) => {
+  if (err) throw err;
+  console.log('Connected!');
+});
+
+connection.end((err) => {
+  // The connection is terminated gracefully
+  // Ensures all remaining queries are executed
+  // Then sends a quit packet to the MySQL server.
+});
 
 app.use('/auth', authRoutes);
 app.get("/", (req, res) => {
